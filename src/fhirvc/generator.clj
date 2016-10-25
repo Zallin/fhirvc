380157876b8a59d3cf6a99e388714e329d0bb760
Tim Zallin <tim.zallin@gmail.com>
2016-10-25 18:30:40 +0300

    views work with data directly and generator preps it for them

--- src/fhirvc/generator.clj
+++ src/fhirvc/generator.clj
@@ -1,25 +1,52 @@
 (ns fhirvc.generator
-  (:require [fhirvc.views :refer :all]
-            [fhirvc.adt :refer :all]
-            [clojure.string :refer [replace]]
+  (:require [fhirvc.views :as views]
+            [fhirvc.diff :as diff]
+            [fhirvc.comp :as comp]
             [me.raynes.fs :refer [copy-dir mkdir]]))
 
+(defn generate-page
+  [path-to-page view data]
+  (spit path-to-page (view data)))
+
+(defn index-view-data [comparison]
+  (let [[a b] (comp/names comparison)]
+    [a b (comp/ref comparison)]))
+
 (defn generate-index [output-folder comp-seq]
-  (spit (str output-folder "/index.html") (index comp-seq)))
+  (generate-page (str output-folder "/index.html")
+                 views/index
+                 (map index-view-data comp-seq)))
 
-(defn generate-comparison-page [output-folder comp]
-  (spit (str output-folder "/" (comp-ref comp))
-        (version-comparison comp)))
+(defn name-ref-pair [comparison difference]
+  [(diff/name difference) (comp/diff-ref comparison difference)])
+  
+(defn comparison-view-data [comparison]
+  (let [difference (comp/diff comparison)]
+    (concat (comp/names comparison)
+            (map #(map (partial name-ref-pair comparison) %)
+                 (diff/enumerate difference)))))
 
-(defn generate-definition-page [output-folder comp]
-  (let [diff (fhir-diff comp)
-        folder-name (str output-folder "/" (replace (comp-ref comp) "_page.html" "" ))]
+(defn generate-comparison-page [output-folder comparison]
+  (generate-page (str output-folder "/" (comp/ref comparison))
+                 views/version-comparison
+                 (comparison-view-data comparison)))
+
+(defn definition-view-data [difference]
+  (vector (diff/type difference)
+          (diff/name difference)
+          difference))
+
+(defn generate-definition-page [output-folder comparison difference]
+  (generate-page (str output-folder "/" (comp/diff-ref comparison difference))
+                 views/definition
+                 (definition-view-data difference)))
+
+(defn generate-definition-pages [output-folder comparison]
+  (let [difference (comp/diff comparison)
+        folder-name (str output-folder "/" (comp/folder comparison))]
     (mkdir folder-name)
-    (dorun (map #(spit (str folder-name "/" (def-name %) ".html") (definition %))
-                (concat (added diff)
-                        (removed diff)
-                        (changed diff)
-                        (unchanged diff))))))
+    (dorun (map (partial generate-definition-page output-folder comparison)                   
+                (reduce concat (diff/enumerate difference))))))                       
 
 (defn move-static-to [output-folder]
   (copy-dir "resources/static/css" (str output-folder "/css")))
@@ -30,5 +57,5 @@
   (generate-index output-folder comp-seq)
   (dorun (map (fn [comp]
                 (generate-comparison-page output-folder comp)
-                (generate-definition-page output-folder comp))
+                (generate-definition-pages output-folder comp))
               comp-seq)))
--- src/fhirvc/views.clj
+++ src/fhirvc/views.clj
@@ -1,8 +1,7 @@
 (ns fhirvc.views
   (:require [hiccup.core :as hc]
             [hiccup.page :as page]
-            [clojure.string :refer [replace lower-case]]
-            [fhirvc.adt :refer :all]
+            [clojure.string :refer [lower-case]]
             [json-html.core :refer :all]
             [config.core :refer [env]]))
 
@@ -25,41 +24,41 @@
 (defn index [comp-seq]
   (layout "FHIRvc | Choose versions to compare"
           [:div.row
-           [:h3 "Following versions currenly supported"]]          
+           [:h3 "Following versions currenly supported"]]
           [:div.row 
            [:div.large-10.medium-10 
             [:form
              [:div.row
               [:ul
-               (for [comp comp-seq]
-                 (let [[a b] (fhir-names comp)]
-                   [:li [:a {:href (comp-ref comp)} (str a " to " b)]]))]]]]]))
+               (for [[a b ref] comp-seq]
+                 [:li [:a {:href ref} (str a " to " b)]])]]]]]))
 
-(defn section [header defs comp]
+(defn section [header defs]
   (if (empty? defs)
     [:div.row
      [:h4 (str "No " (lower-case header))]]
     [:div.row
      [:h4 header]
      [:ul
-      (for [def defs]
-        [:li [:a {:href (def-ref def comp)} (def-name def)]])]]))
+      (for [[name ref] defs]
+        [:li [:a {:href ref} name]])]]))
 
 (defn version-comparison [comparison]
-  (let [diff (fhir-diff comparison)]
+  (let [[a b added removed changed unchanged]
+        comparison]
     (layout "FHIRvc | Comparison summary"
             [:div.row
-             (let [[a b] (fhir-names comparison)]
-               [:h3 (str a " compared to " b)])]
-            (section "Added definitions" (added diff) comparison)
-            (section "Removed definitions" (removed diff) comparison)
-            (section "Changed defitinions" (changed diff) comparison)
-            (section "Unchanged definitions" (unchanged diff) comparison))))
+             [:h3 (str a " compared to " b)]]
+            (section "Added definitions" added)
+            (section "Removed definitions" removed)
+            (section "Changed defitinions" changed)
+            (section "Unchanged definitions" unchanged))))
 
 (defn definition [def]
-  (layout "FHIRvc | Definition"
-          [:div.row
-           [:h3 (str "Resource type: " (def-type def))]
-           [:h3 (str "Resource name: " (def-name def))]]
-          [:div.row
-           (edn->html def)]))
+  (let [[type name cnt] def]
+    (layout "FHIRvc | Definition"
+            [:div.row
+             [:h3 (str "Resource type: " type)]
+             [:h3 (str "Resource name: " name)]]
+            [:div.row
+             (edn->html cnt)])))
