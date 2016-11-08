e8d32c7df0374c051b954b6486a3a56cd88f2284
Tim Zallin <tim.zallin@gmail.com>
2016-11-08 15:10:28 +0300

    new representation

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,11 +1,8 @@
 $(function() {
-    $('.tree').jstree({
-	"core" : {
-	    "themes" : {
-		"dots" : false,
-		"icons" : false
-	    }
-	}
+    $('.tree li > p').click(function (e){
+	e.preventDefault()
+	var childUl = $(this).parent().children('ul')
+	var newVal = childUl.css('display') == 'none' ? 'block' : 'none';
+	childUl.css('display', newVal);
     });
 });
-
