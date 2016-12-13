01863edfcbff42e984df6926dd1f9dfe7a891f66
tim_zallin <tim.zallin@gmail.com>
2016-12-13 18:02:04 +0300

    config dependency removed

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [hiccup "1.0.5"]
-                 [yogthos/config "0.8"]
                  [cheshire "5.6.3"]
                  [json-html "0.4.0"]
                  [me.raynes/fs "1.4.6"]
@@ -14,6 +13,4 @@
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler fhirvc.handler/app}
   :main fhirvc.core
-  :profiles
-  {:dev {:dependencies [[im.chit/vinyasa "0.4.7"]] :resource-paths ["config/dev"]}
-   :prod {:resource-paths ["config/prod"]}})
+  :profiles {:dev {:dependencies [[im.chit/vinyasa "0.4.7"]]}})       
